Fix off-by-one error where the last LED in a row never came on

src/unicorn_driver.py
@@ -17,7 +17,7 @@ class UnicornDriver:
         unicornhat.set_layout(unicornhat.AUTO)
         unicornhat.rotation(270)
         unicornhat.clear()
-        unicornhat.brightness(.2)
+        unicornhat.brightness(.5)
         width,height=unicornhat.get_shape()
         self.ROW_LENGTH = height
         self._leds = np.zeros((width,height), np.bool)
@@ -30,8 +30,8 @@ class UnicornDriver:
         Set one row as a bar graph
         """
         #print(f"XXX setting {row} to {how_many}")
-        if how_many >= self.ROW_LENGTH:
-            how_many = self.ROW_LENGTH-1
+        if how_many > self.ROW_LENGTH:
+            how_many = self.ROW_LENGTH
 
         # Set the row as a bar graph
         for i in range(self.ROW_LENGTH):
@@ -57,7 +57,7 @@ class UnicornDriver:
         max = self._round_up(float(recv_mbps))
         if max == 0:
             max = 1
-        count = round(float(recv_mbps)*float(self.ROW_LENGTH)/float(max))
+        count = round(float(recv_mbps)*float(0.5+self.ROW_LENGTH)/float(max))
         
         #print(f"XXX bars is {tens_count} {count} for mbps {recv_mbps} with max {max}")
         self._set_row(self.NET_IN_ONES_ROW, count)
@@ -68,11 +68,11 @@ class UnicornDriver:
         max = self._round_up(float(sent_mbps))
         if max == 0:
             max = 1
-        count = round(float(sent_mbps)*float(self.ROW_LENGTH)/float(max))
+        count = round(float(sent_mbps)*float(0.5+self.ROW_LENGTH)/float(max))
         self._set_row(self.NET_OUT_ONES_ROW, count)
 
-        self.prev_sent = sent
-        self.prev_recv = recv
+        self.prev_sent = sent_mbps
+        self.prev_recv = recv_mbps
 
     def refresh(self):
         unicornhat.show()