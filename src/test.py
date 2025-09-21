7598509e4835527927f72e9cf1dcf3e266d8cfe3
root <root@OpenWrt.lan>
2025-09-21 04:13:52 +0000

    Display status from remote server

--- src/test.py
+++ src/test.py
@@ -1,31 +1,71 @@
-import stats
+import eventlet
 import gevent
-import unicorn_driver
+import socketio
+import stats
+import threading
+import time
+import unicorn_mini_driver
 
 WINDOW_SECONDS = 1
 s = stats.Stats(10, WINDOW_SECONDS)
 
-u = unicorn_driver.UnicornDriver()
+global last_timer_time
+global last_timer_cleared
+last_timer_time = 0
+last_timer_timeout = 5
+last_timer_cleared = True
 
+#u = unicorn_driver.UnicornDriver()
+u = unicorn_mini_driver.UnicornMiniDriver()
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+    
+@sio.event
+def connect(sid, environ):
+    print(f"Got remote io connection from {sid}")
+
+
+@sio.event
+def set_timer(sid, data):
+    print(f"Got timer data len {len(data)}")
+    global last_timer_time
+    global last_timer_cleared
+    last_timer_time = time.time()
+    last_timer_cleared = False
+
+    u.display_raw(data)
+
+def serve_app(sio, app):
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('192.168.1.1', 5000)), app)
+
+wst = threading.Thread(target=serve_app, args=(sio,app))
+wst.daemon = True
+wst.start()
+print("Daemon started")
 
 while True:
-    #sysload = s.get_load()
-    (net_sent, net_recv) = s.get_network("eth0")
-    #temperature = s.get_temperature()
-    #sysmem = s.get_memory_pct()
+    if not last_timer_cleared and time.time() - last_timer_time > last_timer_timeout:
+        u.clear_display()
+        u.enable_network_display()
+        last_timer_cleared = True
+    
+    (net_sent, net_recv) = s.get_network("eth1")
 
-    #print("Load: " + str(sysload))
-    #print("Mem: " + str(sysmem))
     in_MBs = round(float(net_recv)/1024.0/float(WINDOW_SECONDS))
     out_MBs = round(float(net_sent)/1024.0/float(WINDOW_SECONDS))
     in_mbit = in_MBs*8
     out_mbit = out_MBs*8
-    #print("Net: in=" + str(in_mbit) + " out=" + str(out_mbit))
-    #print("Temp: " + str(temperature))
+
 
     u.set_network(out_mbit, in_mbit)
+
+    cpu_speed = s.get_cpu()
+    u.set_cpu(cpu_speed)
+    
     u.refresh()
 
-    prev_sent = out_mbit
-    prev_recv = in_mbit
     gevent.sleep(.1)
---
+++ src/unicorn_mini_driver.py
@@ -0,0 +1,124 @@
+from unicornhatmini import UnicornHATMini
+
+import math
+import numpy as np
+import threading
+
+class UnicornMiniDriver:
+    NET_IN_TENS_ROW = 6
+    NET_IN_ONES_ROW = 5
+    NET_OUT_TENS_ROW = 3
+    NET_OUT_ONES_ROW = 2
+    CPU_ROW = 0
+
+    CPU_MIN = 600
+    CPU_MAX = 1500
+
+
+    def __init__(self):
+        self.unicornhatmini = UnicornHATMini()
+        #unicornhat.set_layout(unicornhat.AUTO)
+        self.unicornhatmini.set_rotation(90)
+        self.unicornhatmini.clear()
+        self.unicornhatmini.set_brightness(.05)
+        width,height = self.unicornhatmini.get_shape()
+        self.ROW_LENGTH = height
+        self.COL_LENGTH = width
+        self._leds = np.zeros((width,height), np.bool)
+
+        self.CPU_MHZ_PER_PIXEL = float((self.CPU_MAX-self.CPU_MIN))/float(self.ROW_LENGTH)
+        self.prev_sent = 0
+        self.prev_recv = 0
+
+        self._disable_network_display = False
+
+        self._raw_lock = threading.Lock()
+        
+    def _set_row(self, row, how_many, color):
+        """
+        Set one row as a bar graph
+        """
+        #print(f"XXX setting {row} to {how_many}")
+        if how_many > self.ROW_LENGTH:
+            how_many = self.ROW_LENGTH
+
+        # Set the row as a bar graph
+        for i in range(self.ROW_LENGTH):
+            # if we should turn on the LED
+            if i < how_many:
+                self._leds[row][i] = True
+                self.unicornhatmini.set_pixel(row, i, color[0], color[1], color[2])
+            # if we should turn off the LED
+            elif i >= how_many:
+                self._leds[row][i] = False
+                self.unicornhatmini.set_pixel(row, i, 0, 0, 0)
+
+    def set_network(self, sent, recv):
+        if self._disable_network_display:
+            return
+        
+        recv_mbps = float(recv)/1024.0
+        tens_count = round(recv_mbps*float(self.ROW_LENGTH)/100.0)
+        self._set_row(self.NET_IN_TENS_ROW, tens_count, (0, 255 ,0))
+        max = self._round_up(float(recv_mbps))
+        if max == 0:
+            max = 1
+        count = round(float(recv_mbps)*float(0.5+self.ROW_LENGTH)/float(max))
+        
+        #print(f"XXX bars is {tens_count} {count} for mbps {recv_mbps} with max {max}")
+        self._set_row(self.NET_IN_ONES_ROW, count, (0, 255 ,0))
+        self._set_row(self.NET_IN_ONES_ROW-1, count, (0, 255 ,0))
+
+        sent_mbps = float(sent)/1024.0
+        tens_count = round(sent_mbps*float(self.ROW_LENGTH)/100.0)
+        self._set_row(self.NET_OUT_TENS_ROW, tens_count, (0, 0, 255))
+        max = self._round_up(float(sent_mbps))
+        if max == 0:
+            max = 1
+        count = round(float(sent_mbps)*float(0.5+self.ROW_LENGTH)/float(max))
+        self._set_row(self.NET_OUT_ONES_ROW, count, (0, 0, 255))
+        self._set_row(self.NET_OUT_ONES_ROW-1, count, (0, 0, 255))
+
+        self.prev_sent = sent_mbps
+        self.prev_recv = recv_mbps
+
+    def set_cpu(self, cpu_speed):
+        if self._disable_network_display:
+            return
+        how_many = round(float(cpu_speed - self.CPU_MIN) / self.CPU_MHZ_PER_PIXEL)
+        self._set_row(self.CPU_ROW, how_many, (255, 0, 0))
+
+    def clear_display(self):
+        for i in range(self.COL_LENGTH):
+            for j in range(self.ROW_LENGTH):
+                self.unicornhatmini.set_pixel(i, j, 0, 0, 0)
+
+        self.refresh()
+        
+    def refresh(self):
+        self.unicornhatmini.show()
+
+    def _round_up(self, val):
+        return int(math.ceil(float(val)/5.0)) * 5
+
+    def enable_network_display(self):
+        self.unicornhatmini.set_rotation(90)
+        self._disable_network_display = False
+        
+    def display_raw(self, data):
+        with self._raw_lock:
+            self._disable_network_display = True
+            self.unicornhatmini.set_rotation(0)
+        
+            for i in range(self.COL_LENGTH):
+                print("")
+                for j in range(self.ROW_LENGTH):
+                    if data[i*self.ROW_LENGTH + j]:
+                        print("X",end='')
+                        self.unicornhatmini.set_pixel(j, i, 0, 255, 0)
+                    else:
+                        print(".",end='')
+                        self.unicornhatmini.set_pixel(j, i, 0, 0, 0)
+            print("")
+            self.refresh()
+                    
